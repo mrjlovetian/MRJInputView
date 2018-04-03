aea5992ed2e5d159869896f865345d0d15f72158
余洪江 <>
2018-04-03 10:21:08 +0800

    Initial commit

---
+++ Example/MRJInputView/MRJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MRJAppDelegate.h
+//  MRJInputView
+//
+//  Created by mrjlovetian@gmail.com on 04/03/2018.
+//  Copyright (c) 2018 mrjlovetian@gmail.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface MRJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/MRJInputView/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MRJInputView
+//
+//  Created by mrjlovetian@gmail.com on 04/03/2018.
+//  Copyright (c) 2018 mrjlovetian@gmail.com. All rights reserved.
+//
+
+@import UIKit;
+#import "MRJAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MRJAppDelegate class]));
+    }
+}
